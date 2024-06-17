514b660e348e10504c1e0867d1f6d8ec867c5cc2
A. Evrade <97769877+Evrade@users.noreply.github.com>
2024-06-17 15:58:55 -0800

    Add files via upload

--- flaskbot.py
+++ flaskbot.py
@@ -3,13 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-def home_page():
-    return "<h1>Home page</h1>"
-
-@app.route("/about")
-def about():
-    return "<h1>About page</h1>"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello_world():
+    return "Hello, World!"
